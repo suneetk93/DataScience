1d54f7969a120df497767e2d4878df28b7e2aaaa
suneetk93 <65084257+suneetk93@users.noreply.github.com>
2020-05-09 22:41:07 +0530

    Add files via upload

---
+++ SKjuputer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Data Science Experience
+
+# **Suneet Kalyanam** 
+# <br> Aspiring Data Scientist  
+
+# *I am interrested in data science because I like telling stories with data <br>
+# by converting them in a presentable manner so that an organisation can take <br> 
+# informed decisions about it's future. Moreover I have always been a technophile,<br>
+# I love these latest technologies. I like to try these new technologies and see <br> 
+# how it can help humanity for the better.*
+# 
+
+# ### Below is a code which shows the summation of 2 and 3.
+
+# In[1]:
+
+
+2+3
+
+
+# ~~This is a strike through text~~ <br>
+# [Inline style link of Google](https://www.google.com) <br>
+# ***
+# 
+
+# In[ ]:
+
+
+
+
